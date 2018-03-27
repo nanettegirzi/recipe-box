a01f2e8fcec25e59ca9a5e52c4bbef75204308fa
Nanette Girzi <ngirzi@gmail.com>
2018-03-27 10:04:00 -0700

    initial file, project set up

---
+++ recipe-box/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = "Nano's Recipe Box";
+  currentRecipe = 
+  currentTime = new Date ();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+}
